ce66db636c2f8e83aeb73d46c5aa097ee6a31bdc
Daninext <47050120+Daninext@users.noreply.github.com>
2022-11-30 09:51:19 +0300

    feat: upload plagin

---
+++ SpeedMeter/InputFilter.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Editor;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.TextManager.Interop;
+using Microsoft.VisualStudio.Utilities;
+using System;
+using System.ComponentModel.Composition;
+using System.Runtime.InteropServices;
+
+namespace SpeedMeter
+{
+    [Export(typeof(IVsTextViewCreationListener))]
+    [TextViewRole(PredefinedTextViewRoles.Editable)]
+    [ContentType("text")]
+    internal class InputFilter : IVsTextViewCreationListener
+    {
+        [Import]
+        internal IVsEditorAdaptersFactoryService AdapterService = null;
+
+        internal SpeedAdornment adornment;
+
+        public void VsTextViewCreated(IVsTextView textViewAdapter)
+        {
+            ITextView textView = AdapterService.GetWpfTextView(textViewAdapter);
+            if (textView == null)
+                return;
+
+            adornment = textView.Properties.GetProperty<SpeedAdornment>(typeof(SpeedAdornment));
+
+            textView.Properties.GetOrCreateSingletonProperty(() => new CharFilter(textViewAdapter, textView, adornment));
+        }
+    }
+
+    internal sealed class CharFilter : IOleCommandTarget
+    {
+        IOleCommandTarget nextCommandHandler;
+        SpeedAdornment adornment;
+        ITextView textView;
+
+        internal CharFilter(IVsTextView adapter, ITextView textView, SpeedAdornment adornment)
+        {
+            this.textView = textView;
+            this.adornment = adornment;
+
+            adapter.AddCommandFilter(this, out nextCommandHandler);
+        }
+
+        public int Exec(ref Guid pguidCmdGroup, uint nCmdID, uint nCmdexecopt, IntPtr pvaIn, IntPtr pvaOut)
+        {
+            Microsoft.VisualStudio.Shell.ThreadHelper.ThrowIfNotOnUIThread();
+            int hr = VSConstants.S_OK;
+
+            char typedChar;
+            if (TryGetTypedChar(pguidCmdGroup, nCmdID, pvaIn, out typedChar))
+            {
+                adornment.UpdateValues(typedChar);
+            }
+
+            hr = nextCommandHandler.Exec(ref pguidCmdGroup, nCmdID, nCmdexecopt, pvaIn, pvaOut);
+
+            return hr;
+        }
+
+        public int QueryStatus(ref Guid pguidCmdGroup, uint cCmds, OLECMD[] prgCmds, IntPtr pCmdText)
+        {
+            Microsoft.VisualStudio.Shell.ThreadHelper.ThrowIfNotOnUIThread();
+            return nextCommandHandler.QueryStatus(ref pguidCmdGroup, cCmds, prgCmds, pCmdText);
+        }
+
+        bool TryGetTypedChar(Guid cmdGroup, uint nCmdID, IntPtr pvaIn, out char typedChar)
+        {
+            typedChar = char.MinValue;
+
+            if (cmdGroup != VSConstants.VSStd2K || nCmdID != (uint)VSConstants.VSStd2KCmdID.TYPECHAR)
+                return false;
+
+            typedChar = (char)(ushort)Marshal.GetObjectForNativeVariant(pvaIn);
+            return true;
+        }
+    }
+}
---
+++ SpeedMeter/ShowSpeed.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+using System;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Design;
+using System.Globalization;
+using Task = System.Threading.Tasks.Task;
+
+namespace SpeedMeter
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal class ShowSpeed
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("598d6430-d476-42a4-9498-197275b666ad");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ShowSpeed"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private ShowSpeed(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static ShowSpeed Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in ShowSpeed's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new ShowSpeed(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            //string message = sender.ToString();
+            string message = string.Format(CultureInfo.CurrentCulture, "Char/min: {0} \nWord/min: {1} \nTotal words: {2}", SpeedAdornment.instance.CharSpeed, SpeedAdornment.instance.WordSpeed, SpeedAdornment.instance.TotalWords);
+            string title = "Total Speed";
+
+            // Show a message box to prove we were here
+            VsShellUtilities.ShowMessageBox(
+                this.package,
+                message,
+                title,
+                OLEMSGICON.OLEMSGICON_INFO,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+    }
+}
